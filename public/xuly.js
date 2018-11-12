e344f944019991003bdbee7cfbcb5ae24203640c
MinhVuongTran <minhvuonga31996@gmail.com>
2018-11-12 12:31:33 +0700

    intital commit

---
+++ public/xuly.js
@@ -0,0 +1,91 @@
+
+var socket = io("http://localhost:4000");
+
+var str0 = document.cookie;
+        var nameofcookies = document.cookie.substring(0,str0.indexOf("=") );
+        
+        var str = document.cookie.substring(str0.indexOf("=") + 1);
+        var arr = str.split("%20");
+        var userNameofMe = arr[0];
+        var nameofMe = "";
+        for(var i =1;i<arr.length; i++){
+            nameofMe +=arr[i] +" "; 
+        }
+socket.emit("myname", userNameofMe);
+
+
+socket.on("server-send-danhsach-users", function(data){
+    $("#boxContent").html("");
+    data.us.forEach(function(i){
+        var j = 0, check =0;
+        for(j=0; j<data.arr.length ; j++)
+            if(i.userName ==data.arr[j] )
+            check++;
+       
+        var str='' ;
+        str+= '<div><a href="/messenger/';
+        str+= i.userName + '">';
+        if(check){
+            if(i.userName != userNameofMe)
+        $("#boxContent").append(str+'<div class = "user"  style="float: left" data-info ='+ i.userName+'>'+i.name+'</div></a><div><div style="float:right;height:10px; width:10px;margin-top:20px; background:red;border-radius:20px"></div></div></div>');
+        }
+        else
+        {
+        if(i.userName != userNameofMe)
+        $("#boxContent").append(str+'<div class = "user" data-info ='+ i.userName+'>'+i.name+'</div></a>');
+    }
+    });
+});
+socket.on("Server-say-ok", function(data){
+    console.log(data);
+    $("#listMessenger").html("");
+   
+        if(data.sender === userNameofMe){
+            $("#listMessenger").append("<div class='me'><p style='color:white; margin-bottom:0.5rem;'>"+data.content+"</p></div>");}
+            else{
+                $("#listMessenger").append("<div class='you'><p style='color:white;margin-bottom:0.5rem; '>"+data.content+"</p></div>");
+            } 
+    
+});
+socket.on("server-chat", function(data){
+    console.log(data);
+    var arr = data.split(":");
+    console.log(arr[1]);
+    if(arr[0]=== userNameofMe){
+    $("#listMessenger").append("<div class='me'><p style='color:white; margin-bottom:0.5rem;'>"+arr[1]+"</p></div>");}
+    else{
+        $("#listMessenger").append("<div class='you'><p style='color:white;margin-bottom:0.5rem; '>"+arr[1]+"</p></div>");
+    }
+
+});
+$(document).ready(function()
+    {
+        socket.emit("mychat", userNameofMe + ","+$("#useName").attr('data-name'));
+        $("#btnLogOut").click(function(){
+            socket.emit("client-send-logout"); 
+        });
+      
+        $("#_id").append("<u>" + nameofMe+"</u>")
+        $("#btnSend").click(function(){
+            if($("#txtMessage").val().trim())
+           
+           socket.emit("user-send-message", $("#useName").attr('data-name')+":"+userNameofMe+": "+ $("#txtMessage").val().trim());
+        });
+        // $("#txtMessage").focusin(function(){
+        //     socket.emit("user-dang-go", );
+        //  });
+        //  $("#txtMessage").focusout(function(){
+        //     socket.emit("user-khong-go", );
+        //  });
+        $(document).on('mouseover', '.user', function(e) {
+            $(this).css('background-color','aqua');
+        });
+        $(document).on('click', '.user', function(e) {
+            var a = $(this).html();
+            socket.emit("socket-send-taoroom", userNameofMe+","+ $(this).attr('data-info'));
+        });
+        
+        $(document).on('mouseout', '.user', function(e) {
+            $(this).css('background-color','#e9ebee');
+        });
+    });
---
+++ server.js
@@ -0,0 +1,183 @@
+var express = require("express");
+var cookieParser = require("cookie-parser");
+var mongoose = require('mongoose');
+var app = express();
+var bodyParser = require("body-parser");
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.set("views", "./views");
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended : true
+}));
+// mongoose.connect("mongodb://localhost:4000");
+// var userSchema = new mongoose.Schema({
+//     userName: String,
+//     pass: String,
+//     name: String
+// });
+// var msmSchema = new mongoose.Schema({
+//     content: String,
+//     sender:String,
+//     reer:String
+// })
+var low = require("lowdb");
+var FileSync = require("lowdb/adapters/FileSync");
+var adapter = new FileSync("db.json");
+var arrayOnline = [];
+db =low(adapter);
+db.defaults({users: []}).write();
+db.defaults({msm: []}).write();
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
+server.listen(4000);
+
+
+
+io.on("connection", function(socket){
+    var tmp;
+   socket.on("myname", function(data){
+       socket.id_user  = data||"";
+       tmp =socket.id_user;
+       if(socket.id_user) arrayOnline.push(socket.id_user);
+       io.sockets.emit("server-send-danhsach-users",{us : db.get("users").value() , arr: arrayOnline});
+    });  
+   socket.on("disconnect", function(){
+       arrayOnline.forEach(i => {
+           if(i === tmp){
+            arrayOnline.splice(tmp,1); 
+           }
+       });
+       socket.broadcast.emit("server-send-danhsach-users",{us : db.get("users").value() , arr: arrayOnline});
+       
+   });
+   socket.on("logout", function(){
+    arrayOnline.splice(tmp,1);
+    socket.broadcast.emit("server-send-danhsach-users",{us : db.get("users").value() , arr: arrayOnline});
+   });
+  
+   
+    
+    socket.on("mychat", function(data){
+        console.log(data);
+        var arrrom = data.split(",");
+        socket.join(arrrom[0]+","+arrrom[1]);
+        socket.join(arrrom[1]+","+arrrom[0]);
+        socket.Phong = data;
+        socket.phong = arrrom[1]+","+arrrom[0];
+        var mesages = db.get('msm').filter({sender : arrrom[0], received :arrrom[1]}||{sender : arrrom[1], received :arrrom[0]}).value();
+        console.log(mesages);
+        socket.emit("Server-say-ok", mesages);
+        
+       
+    });
+    socket.on("user-send-message",function(data){
+        var ar = data.split(":");
+        console.log(ar[1]);
+        socket.join(ar[0]+","+ar[1]);
+        socket.join(ar[1]+","+ar[0]);
+        socket.phong = ar[0]+","+ar[1];
+        io.sockets.in(socket.phong).emit("server-chat", ar[1]+":"+ ar[2]);
+    });
+    
+});
+
+app.get("/" ,function(req,res){
+    
+   
+    if(!req.cookies.name){
+    res.redirect("/index");
+    return;
+    }
+    var coo= req.cookies.name;
+    var coo2 = [];
+    coo2 = coo.split(' ');
+    var i =0;
+    for(i=2; i< coo2.length; i++)
+        coo2[1] += " "+coo2[i];
+    var usera =db.get('users').find({userName:coo2[0], name:coo2[1]     }).value();
+    if(!usera){
+        res.redirect("/index");
+        return;
+    }
+    res.redirect("/messenger");
+});
+app.get("/index", function(req, res){
+    res.render("index");
+});
+app.get("/create", function(req, res){
+    res.render("create");
+});
+app.post("/index", function(req, res){
+    //sẽ xử lý login chổ này
+   
+        var userName = req.body.userName.trim();
+        var pass=req.body.pass.trim();
+        var errors = [];
+        if(userName ==="" || pass==="") errors.push("Tên đăng nhập hoặc mật khẩu không được để trống!");
+        else{
+        if(!(db.get("users").find({userName :userName}).value())) 
+            errors.push("Tên đăng nhập không đúng!");
+        if(!(db.get("users").find({pass :pass}).value() ))
+           errors.push("Mật khẩu không chính xác!");
+        }  
+        if(errors.length){
+            res.render('index', {
+                errors : errors
+            });
+            return;
+        };
+        var coo =  userName +" "+ (db.get("users").find({userName :userName}).value().name);
+        res.cookie('name',coo, {expire: 360000 + Date.now()}); 
+        
+    res.redirect("/messenger");
+});
+app.post("/create", function(req, res){
+    var errors = [];
+    var userName = req.body.userName.trim();
+    if(!req.body.userName.trim())
+        errors.push("Vui lòng nhập tên đăng nhập");
+    if(db.get("users").find({userName :userName}).value())
+        errors.push("Tài khoản đã tồn tại");
+        else{
+    if(!req.body.name.trim())
+        errors.push("Vui lòng nhập tên hiển thị");
+    if(!req.body.pass.trim())
+        errors.push("Vui lòng nhập mật khẩu");
+        }
+    if(errors.length){
+        if(errors.length){
+            res.render('create', {
+                errors : errors,
+                
+            });
+            return;
+        };
+    }
+    db.get("users").push({userName : req.body.userName.trim(),name : req.body.name.trim(),pass : req.body.pass.trim()}).write();
+    var coo =  userName +" "+ (db.get("users").find({userName :userName}).value().name);
+    res.cookie('name',coo, {expire: 360000 + Date.now()}); 
+    res.redirect("/messenger");
+            
+
+   
+});
+
+app.get("/messenger/:userName", function(req, res){
+    var userName = req.params.userName;
+    var user = db.get("users").find({userName :userName}).value();
+    if(user)
+    res.render("messenger/view",{
+        user : user
+    });
+    else 
+    res.render("messenger");
+});
+app.get("/messenger/", function(req, res){
+    res.render("messenger");
+});
+app.get("/messenger", function(req, res){
+    res.render("messenger");
+});
+console.log(db.get("users").value());
